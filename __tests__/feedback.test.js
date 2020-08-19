4c149d93e467ccc7f2a5c90f8b9f316f4b640278
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2020-08-19 19:21:20 -0400

    jest testing of feedback detection partially completed

--- __tests__/feedback.test.js
+++ __tests__/feedback.test.js
@@ -14,23 +14,84 @@ describe('feedback path detection', () => {
     })
     beforeEach(() => {
       /* Runs before each test */
-      d = JSON.parse(fs.readFileSync('__tests__/deltas/scene_feedback.json'))
-      g = got.graphFromDeltas(d)
-      g1 = got.graphFromDeltas(d)
+      
+      g = JSON.parse(fs.readFileSync('__tests__/deltas/scene_feedback.json'))
+    //   g1 = got.graphFromDeltas(d)
     })
     afterEach(() => {
       /* Runs after each test */
     })
 
-    test('find a feedback path', () => {
-        // expect(typeof got.deltasToString(d)).toBe('string');
-        g = got.graphFromDeltas(d)
-        g1 = got.graphFromDeltas(d)
+    test('list all nodes in graph', () => {
+        // console.log(g.nodes)
+        let expected =  [ 
+            'lfo_1.sine',
+            'lfo_1.phasor',
+            'lfo_1.pulse',
+            'lfo_1.sine_index',
+            'lfo_1.saw',
+            'lfo_2.sine',
+            'lfo_2.phasor',
+            'lfo_2.pulse',
+            'lfo_2.sine_index',
+            'lfo_2.saw',
+            'dualvco_1.vco_1',
+            'dualvco_1.vco_2',
+            'dualvco_1.master',
+            'vca_1.output',
+            'pulsars_1.output' 
+        ]
+        let nodes = got.getNodes(g)
+        // console.log(nodes)
+        expect(got.deepEqual(expected, nodes)).toBe(true)
+    })
 
-        // expect(typeof g).toBe('object');
-        // expect(g).toMatchObject(simpleSceneSuccess);
-        // expect(got.deepEqual(g, g1)).toBe(true)
-        // expect(got.deepEqual(got.deltasFromGraph(g, []), got.deltasFromGraph(g1, []))).toBe(true)
+    
+    test('list all adjacent nodes in graph', () => {
+        // expect(typeof got.deltasToString(d)).toBe('string');
+        // g = got.graphFromDeltas(d)
+        let nodes =  [ 
+            'lfo_1.sine',
+            'lfo_1.phasor',
+            'lfo_1.pulse',
+            'lfo_1.sine_index',
+            'lfo_1.saw',
+            'lfo_2.sine',
+            'lfo_2.phasor',
+            'lfo_2.pulse',
+            'lfo_2.sine_index',
+            'lfo_2.saw',
+            'dualvco_1.vco_1',
+            'dualvco_1.vco_2',
+            'dualvco_1.master',
+            'vca_1.output',
+            'pulsars_1.output' 
+        ]
+        let expected = { 
+            lfo_1: [ 
+                [ 'lfo_1.sine', 'vca_1.cv' ] 
+            ],   
+            lfo_2: [ [ 'lfo_2.phasor', 'dualvco_1.index_cv' ],
+            [ 'lfo_2.saw', 'vca_1.signal' ] ],      
+            dualvco_1: [ [ 'dualvco_1.master', 'pulsars_1.signal' ] ],
+            vca_1:
+            [ [ 'vca_1.output', 'dualvco_1.rate_1_cv' ],
+            [ 'vca_1.output', 'pulsars_1.period_cv' ] ],
+            pulsars_1:
+            [ [ 'pulsars_1.output', 'lfo_1.fm_cv' ],  
+            [ 'pulsars_1.output', 'speaker_1.input' ],
+            [ 'pulsars_1.output', 'pulsars_1.formant_cv' ] ] 
+        }
+        let adjacents = got.getAdjacents(0, nodes, g)
+        
+        // console.log(adjacents)
+        expect(got.deepEqual(expected, adjacents)).toBe(true)
+
     });
+
+    test('list all feedback paths in graph', () => {
+        throw('this test not completed yet')
+    })
+
 
 })
